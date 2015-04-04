06ff8823dbf25c6db1ac87af171597a67339c2c4
rnikoopour <rnikoopour@gmail.com>
2015-04-04 15:00:19 -0700

    Sprickled a ton of magic on this.  This is hands down the worst program I've ever written

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,11 +1,15 @@
 import sys
+sys.path.append('../Lib')
+from recursive_decent_parser import Parser
 
-def main():
-    if len(sys.argv[1:]) is 2:
-        Parser(sys.argv[1], sys.argv[2])
+def main(file=None):
+    if file is None:
+        if len(sys.argv[1:]) is 2:
+            Parser(sys.argv[1], sys.argv[2])
+        else:
+            Parser(sys.argv[1])
     else:
-        Parser(sys.argv[1])
-
+            Parser(file)
 if __name__ == '__main__':
     sys.path.append('../Lib')
     from recursive_decent_parser import Parser
